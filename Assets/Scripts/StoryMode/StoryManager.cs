a2378dd5661519e18d6a79209a281f1c69c5eae2
Kither12 <developerkither2509@gmail.com>
2023-03-13 20:57:42 +0700

    update

--- Assets/Scripts/StoryMode/StoryManager.cs
+++ Assets/Scripts/StoryMode/StoryManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StoryManager : MonoBehaviour
 {
@@ -9,19 +10,46 @@ public class StoryManager : MonoBehaviour
     public Canvas Setting;
     public Canvas PauseMenu;
     public Canvas SettingPanel;
+    public Canvas MainCanvas;
+    public GameObject OkayButton;
+    public TextMeshProUGUI Intro;
+    public GameObject globalButton;
+    public float WaitChar = 0.01f;
+    public bool finish;
     private void Start() {
         PlayCanvas.enabled = false;
         Setting.enabled = false;
         PauseMenu.enabled = false;
         SettingPanel.enabled = false;
-        triggerCustomer();
+        CustomerInit.enabled = false;
+        MainCanvas.enabled = false;
+        finish = false;
+        StartCoroutine(triggerCustomer());
     }
-    public void triggerCustomer(){
+    private IEnumerator triggerCustomer(){
+        yield return new WaitForSeconds(0.5f);
         CustomerInit.enabled = true;
+        OkayButton.SetActive(false);
+        string temp = Intro.text;
+        Intro.text = "";
+        foreach(char c in temp){
+            Intro.text += c;
+            if(finish){
+                Intro.text = temp;
+                break;
+            }
+            yield return new WaitForSeconds(WaitChar);
+        }
+        globalButton.SetActive(false);
+        OkayButton.SetActive(true);
+    }
+    public void Skip(){
+        finish = true;
     }
     public void Play(){
         PlayCanvas.enabled = true;
         CustomerInit.enabled = false;
+        MainCanvas.enabled = true;
     }
     public void OpenSetting(){
         Setting.enabled ^= true;
